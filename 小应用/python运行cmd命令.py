4346a0de277dd7961dd4b1314ca78b32fdbe921a
bencyq <876687019@qq.com>
2022-01-02 19:59:15 +0800

    update

--- 小应用/python运行cmd命令.py
+++ 小应用/python运行cmd命令.py
@@ -12,13 +12,17 @@ for text in texts:
     address.append(text)
 print(address)
 servers = []
-fp = open('ping.txt', 'w', encoding='utf-8')
 for city in address:
     for server in city[1:]:
         p = os.popen(f'ping {server}')
         text = p.read()
         print(text)
-        if text.count('请求超时') <= 1:
+        if text.count('请求超时') <= 1 and text.count('找不到') == 0:
             servers.append(server)
         print(servers, len(servers))
 print(servers)
+
+with open('ping.txt','w') as fp:
+    for server in servers:
+        fp.write(server)
+        fp.write('\r\n')
